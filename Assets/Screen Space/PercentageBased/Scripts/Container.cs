dfde2e1d4463f486ac133dd388e55021cd22eecf
Lewis Johnson <lewisjohnsondev@gmail.com>
2017-09-01 17:14:14 +0100

    Added scaled component example

---
+++ Assets/Screen Space/PercentageBased/Scripts/Container.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Screen_Space.PercentageBased.Scripts {
+    [RequireComponent(typeof(RectTransform))]
+    [ExecuteInEditMode]
+    public class Container : MonoBehaviour {
+
+        public float Height {
+            get { return GetComponent<RectTransform>().sizeDelta.y; }
+        }
+
+        public float Width {
+            get { return GetComponent<RectTransform>().sizeDelta.x; }
+        }
+
+        private void Update() {
+            GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+        }
+    }
+}
---
+++ Assets/Screen Space/PercentageBased/Scripts/ScaledComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Screen_Space.PercentageBased.Scripts {
+
+    [RequireComponent(typeof(RectTransform))]
+    [ExecuteInEditMode]
+    public class ScaledComponent : MonoBehaviour {
+
+        public RectTransform Container;
+        public Position ComponentPosition;
+        public Scale ComponentScale;
+
+#if UNITY_EDITOR
+        public GameObject DebugText;
+#endif
+        public void Start() {
+            RefreshUI();
+        }
+
+        private void RefreshUI() {
+
+            float width = (ComponentScale.Width / 100) * Container.GetComponent<Container>().Width;
+            float height = (ComponentScale.Height / 100) * Container.GetComponent<Container>().Height;
+
+            float top = (ComponentPosition.Top / 100) * Container.GetComponent<Container>().Height;
+            float left = (ComponentPosition.Left / 100) * Container.GetComponent<Container>().Width;
+
+            GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+            GetComponent<RectTransform>().anchoredPosition = new Vector3(left, top, 0);
+
+#if UNITY_EDITOR
+            DebugText.GetComponent<Text>().text = PrettyPrint();
+#endif
+        }
+
+        private string PrettyPrint() {
+            return string.Format("Top: {0}%\nLeft: {1}%\nWidth: {2}%\nHeight: {3}%", ComponentPosition.Top, ComponentPosition.Left, ComponentScale.Width, ComponentScale.Height);
+        }
+
+        public void Update() {
+            RefreshUI();
+        }
+    }
+
+    [Serializable]
+    public class Position {
+        public float Left;
+        public float Top;
+    }
+
+    [Serializable]
+    public class Scale {
+        public float Width;
+        public float Height;
+    }
+}
