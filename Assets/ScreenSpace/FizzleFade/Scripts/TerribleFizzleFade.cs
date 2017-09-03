c9637cbc193c52498a07c106aeb4f0b1407c072c
Lewis Johnson <lewisjohnsondev@gmail.com>
2017-09-03 14:38:46 +0100

    Added license headers

--- Assets/ScreenSpace/FizzleFade/Scripts/TerribleFizzleFade.cs
+++ Assets/ScreenSpace/FizzleFade/Scripts/TerribleFizzleFade.cs
@@ -30,10 +30,12 @@ using Random = System.Random;
 
 namespace Assets.ScreenSpace.FizzleFade.Scripts {
 
-    public class FizzleFade : MonoBehaviour {
+    /// <summary>
+    /// Please don't use this. It works but is a hot mess.
+    /// </summary>
+    public class TerribleFizzleFade : MonoBehaviour {
 
-        public Texture2D FizzleTexture2D;
-        private bool _isFilled;
+        public GameObject Image;
 
         public void Start() {
             Fizzle(new Color(255, 0, 0, 255));
@@ -43,44 +45,36 @@ namespace Assets.ScreenSpace.FizzleFade.Scripts {
 
             int width = (int)Math.Ceiling(GetComponent<RectTransform>().sizeDelta.x);
             int height = (int)Math.Ceiling(GetComponent<RectTransform>().sizeDelta.y);
-            FizzleTexture2D = new Texture2D(height, width, TextureFormat.ARGB32, false);
 
-            var fillColorArray = FizzleTexture2D.GetPixels();
-
-            for (var i = 0; i < fillColorArray.Length; ++i) {
-                fillColorArray[i] = new Color(0, 0, 0, 0);
+            var xList = new List<int>();
+            var yList = new List<int>();
+            for (int i = 0; i <= ((int)Math.Round(width / 10.0)) * 10; i += 10) {
+                xList.Add(i);
             }
 
-            FizzleTexture2D.SetPixels(fillColorArray);
-            FizzleTexture2D.Apply();
-            GetComponent<Image>().material.mainTexture = FizzleTexture2D;
+            for (int i = 0; i <= ((int)Math.Round(height / 10.0)) * 10; i += 10) {
+                yList.Add(i);
+            }
 
-            for (int x = 0; x <= width; x++) {
-                for (int y = 0; y <= height; y++) {
-                    //StartCoroutine(WaitAndPrint(x, y, pixelColour, 1));
-                    //FizzleTexture2D.SetPixel(x, y, pixelColour);
+            Shuffle(xList);
+            Shuffle(yList);
+
+            for (int x = 0; x < xList.Count; x++) {
+                for (int y = 0; y < yList.Count; y++) {
+                    var img = Instantiate(Image, transform);
+                    img.GetComponent<Image>().color = pixelColour;
+                    img.GetComponent<RectTransform>().anchoredPosition = new Vector2(xList[x], -yList[y]);
+                    img.SetActive(false);
+                    StartCoroutine(WaitAndPrint(img, (x * y) / 1000));
                 }
-            }
 
-            
+            }
         }
 
 
-        private IEnumerator WaitAndPrint(int x, int y, Color pixelColour, float waitTime) {
-            
-            FizzleTexture2D.SetPixel(x, y, pixelColour);
-            
+        private IEnumerator WaitAndPrint(GameObject k, float waitTime) {
             yield return new WaitForSeconds(waitTime);
-
-        }
-
-        public void FixedUpdate()
-        {
-            if (!_isFilled)
-            {
-                FizzleTexture2D.Apply();
-            }
-            
+            k.SetActive(true);
         }
 
         public static void Shuffle<T>(IList<T> list) {
