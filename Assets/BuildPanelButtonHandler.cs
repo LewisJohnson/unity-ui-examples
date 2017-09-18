c93b118962444503e7286068c9081b595a814105
Lewis Johnson <LewisJohnsonDev@gmail.com>
2017-09-18 14:31:46 +0100

    Improved PA example
    Added description pop up to build panel buttons

---
+++ Assets/BuildPanelButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BuildPanelButtonHandler : MonoBehaviour {
+
+    private Transform _childDescription;
+
+    public void Start () {
+        _childDescription = transform.Find("Description");
+        if(_childDescription == null){
+            Debug.LogError(string.Format("The build panel button \"{0}\" does not have a description.", this.name));
+        }
+	}
+
+    public void onPointerEnter() {
+        _childDescription.gameObject.SetActive(true);
+	}
+
+    public void onPointerLeave(){
+        _childDescription.gameObject.SetActive(false);
+    }
+}
