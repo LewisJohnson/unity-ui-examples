84b63010a5a88df334dfa7263a2c2753d43c47e6
Lewis Johnson <LewisJohnsonDev@gmail.com>
2017-09-18 15:13:23 +0100

    Improved PA example
    - Added action panels children to action panel buttons
    - Renamed prefabs
    - Made time buttons into toggle buttons
    - Added toggle group for time buttons
    - Updated preview image

---
+++ Assets/ScreenSpace/PrisionArchitect/Scripts/BuildPanelButtonHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BuildPanelButtonHandler : MonoBehaviour {
+
+    private Transform _childDescription;
+    private Transform _childActionPanel;
+    public void Start() {
+        _childDescription = transform.Find("Description");
+        _childActionPanel = transform.Find("ActionPanel");
+
+        if (_childDescription == null) {
+            Debug.LogError(string.Format("The build panel button \"{0}\" does not have a description.", this.name));
+        }
+
+        if (_childActionPanel == null) {
+            Debug.LogError(string.Format("The build panel button \"{0}\" does not have an action panel.", this.name));
+        }
+    }
+
+    public void OnPointerEnter() {
+        if (!_childActionPanel.gameObject.activeSelf) {
+            _childDescription.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnPointerLeave() {
+        _childDescription.gameObject.SetActive(false);
+    }
+
+    public void OnPointerClick() {
+        _childDescription.gameObject.SetActive(false);
+
+        if (_childActionPanel.gameObject.activeSelf) {
+            _childActionPanel.gameObject.SetActive(false);
+        } else {
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("ActionPanel")) {
+                go.SetActive(false);
+            }
+            _childActionPanel.gameObject.SetActive(true);
+        }
+    }
+}
