8cf1822144740ec61586bb87a31190e049973bbf
Lewis Johnson <LewisJohnsonDev@gmail.com>
2017-09-21 20:03:11 +0200

    Improved HWUI example
    - Added new images
    - Drastically improved graphics

---
+++ Assets/BindTextToAnotherTextElement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+[ExecuteInEditMode]
+public class BindTextToAnotherTextElement : MonoBehaviour {
+
+    public Text TargetText;
+
+	void Update () {
+        this.GetComponent<Text>().text = TargetText.text;
+	}
+}
